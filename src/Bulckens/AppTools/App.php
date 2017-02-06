1c0e369236574176045e141d90f18d310e833c62
wout <wout@impinc.co.uk>
2017-02-06 20:55:17 +0100

    Reversed back to non grounded Config

--- src/Bulckens/AppTools/App.php
+++ src/Bulckens/AppTools/App.php
@@ -2,15 +2,16 @@
 
 namespace Bulckens\AppTools;
 
+use Exception;
 use Bulckens\AppTraits\Configurable;
-use Bulckens\AppTraits\Grounded;
 
 class App {
 
   use Configurable;
-  use Grounded;
 
   protected static $app;
+  protected static $env;
+  protected static $root;
   protected static $modules;
 
   public function __construct( $env, $root = null ) {
@@ -43,6 +44,47 @@ class App {
     }
 
     return $this;
+  }
+
+
+  // Get host project root
+  public static function root( $path = '' ) {
+    if ( ! self::$root ) {
+      // find root based on the location of the config folder
+      self::$root = __DIR__;
+      $depth = 0;
+
+      // get current config dir
+      $dir = self::env( 'test' ) ? '/dev/config/' : '/config/';
+      
+      // find root dir
+      while ( ! file_exists( self::$root . $dir ) && $depth < 20 ) {
+        // detect capistrano installation
+        if ( basename( dirname( self::$root ) ) == 'shared' )
+          self::$root = dirname( dirname( self::$root ) ) . '/current';
+        else
+          self::$root = dirname( self::$root );
+
+        $depth++;
+      }
+    }
+
+    if ( self::$root == '/' )
+      throw new RootNotFoundException( 'Project root ' . self::$root . ' is not acceptable' );
+
+    return str_replace( '//', '/', self::$root . "/$path" );
+  }
+
+
+  // Test current environment
+  public static function env( $test = null ) {
+    if ( is_null( $test ) )
+      return self::$env;
+
+    elseif ( is_array( $test ) )
+      return in_array( self::$env, $test );
+
+    return $test == self::$env;
   }
 
 
@@ -95,3 +137,6 @@ class App {
   }
 
 }
+
+// Exceptions
+class RootNotFoundException extends Exception {};
