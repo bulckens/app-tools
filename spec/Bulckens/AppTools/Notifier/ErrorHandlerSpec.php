47c4519a9bf84c057252e584b770c85f0268cdc8
wout <wout@impinc.co.uk>
2017-02-14 15:37:42 +0100

    Bug fix en specs for notifier error handler

--- spec/Bulckens/AppTools/Notifier/ErrorHandlerSpec.php
+++ spec/Bulckens/AppTools/Notifier/ErrorHandlerSpec.php
@@ -4,6 +4,8 @@ namespace spec\Bulckens\AppTools\Notifier;
 
 use stdClass;
 use Exception;
+use Slim\Http\Response;
+use Bulckens\Helpers\FileHelper;
 use Bulckens\AppTools\App;
 use Bulckens\AppTools\Notifier\ErrorHandler;
 use PhpSpec\ObjectBehavior;
@@ -12,7 +14,7 @@ use Prophecy\Argument;
 class ErrorHandlerSpec extends ObjectBehavior {
 
   function let() {
-    $app = new App( 'dev', dirname( dirname( dirname( dirname( __DIR__ ) ) ) ) );
+    $app = new App( 'dev', FileHelper::parent( __DIR__, 4 ) );
     $app->run();
   }
   
@@ -21,8 +23,8 @@ class ErrorHandlerSpec extends ObjectBehavior {
     $std = new stdClass();
     $exception = new Exception( 'Fake error' );
 
-    $handler = $this::slim([]);
-    $handler( $std, $std, $exception )->shouldHaveType( 'floo' );
+    $handler = $this::slim([ 'response' => new Response( 200 ) ]);
+    $handler( $std, $std, $exception )->shouldHaveType( 'Slim\Http\Response' );
   }
 
 }
