9f7d85a31194d11a6a644764de1d98a5ca5014dc
wout <wout@impinc.co.uk>
2017-02-14 19:11:35 +0100

    Modularization of app

---
+++ src/Bulckens/AppTools/Traits/Modulized.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Bulckens\AppTools\Traits;
+
+
+trait Modulized {
+
+  protected static $instance;
+  protected static $modules;
+
+  // Get app instance 
+  public static function get() {
+    return self::$instance;
+  }
+
+  // Set and get module instances
+  public function module( $name, $module = null ) {
+    if ( is_null( $module ) ) {
+      if ( isset( self::$modules[$name] ) )
+        return self::$modules[$name];
+    }
+
+    self::$modules[$name] = $module;
+  }
+
+}
