6ba02efc500a168aec6f08f33e1ecdeb790b5b43
wout <wout@impinc.co.uk>
2017-02-19 18:43:39 +0100

    Added Cacheable trait

---
+++ spec/Bulckens/AppTests/TestModelWithCacheableSpec.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace spec\Bulckens\AppTests;
+
+use Bulckens\AppTools\App;
+use Bulckens\AppTests\TestModelWithCacheable;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class TestModelWithCacheableSpec extends ObjectBehavior {
+
+  function let() {
+    $app = new App( 'dev' );
+    $app->run();
+    $this->purgeCache();
+  }
+
+
+  // Cache method
+  function it_stores_a_given_value() {
+    $this->cache()->shouldBe( null );
+    $this->cache( 'falumba' );
+    $this->cache()->shouldBe( 'falumba' );
+  }
+
+  function it_stores_a_given_value_with_a_lifespan() {
+    $this->cache()->shouldBe( null );
+    $this->cache( 'falumba', 1 );
+    sleep( 2 );
+    $this->cache()->shouldBe( null );
+  }
+
+  function it_returns_itself_after_storing_a_given_value() {
+    $this->cache( 'falumba' )->shouldBe( $this );
+  }
+
+  function it_returns_a_stored_value() {
+    $this->cache( 'mafasta' );
+    $this->cache()->shouldBe( 'mafasta' );
+  }
+
+
+  // CacheKey method
+  function it_generates_a_cache_key_including_the_class_name_and_id() {
+    $this->id = 123;
+    $this->cacheKey()->shouldBe( 'bulckens.app_tests.test_model_with_cacheable.123' );
+  }
+
+
+  // Cached methdo
+  function it_tests_positive_when_cached_already() {
+    $this->cache( 'lantra' );
+    $this->cached()->shouldBe( true );
+  }
+
+  function it_tests_negative_when_not_cached() {
+    $this->cached()->shouldBe( false ); 
+  }
+
+
+  // PurgeCache method
+  function it_pureges_the_stored_cache() {
+    $this->cache( 'fickin' );
+    $this->purgeCache();
+    $this->cache()->shouldBe( null );
+  }
+
+  function it_returns_itself_after_purging_the_stored_cache() {
+    $this->purgeCache()->shouldBe( $this );
+  }
+
+}
---
+++ src/Bulckens/AppTools/Traits/Cacheable.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Bulckens\AppTools\Traits;
+
+use Exception;
+use Illuminate\Support\Str;
+use Bulckens\AppTools\App;
+use Bulckens\AppTools\Cache;
+
+trait Cacheable {
+
+  // Set/get cache
+  public function cache( $value = null, $lifespan = null ) {
+    // act as getter
+    if ( is_null( $value ) )
+      return $this->cacheModule()->get( $this->cacheKey() );
+
+    // act as setter
+    $this->cacheModule()->set( $this->cacheKey(), $value, $lifespan );
+
+    return $this;
+  }
+
+
+  // Get unique cache key
+  public function cacheKey() {
+    return str_replace( '\_', '.', Str::snake( get_class() ) ) . ".$this->id";
+  }
+
+
+  // Test existance in cache
+  public function cached() {
+    return $this->cacheModule()->has( $this->cacheKey() );
+  }
+
+
+  // Purge cache from cache store
+  public function purgeCache() {
+    $this->cacheModule()->delete( $this->cacheKey() );
+
+    return $this;
+  }
+
+
+  // Get cache module
+  protected function cacheModule() {
+    if ( $cache = App::get()->cache() )
+      return $cache;
+
+    throw new CacheableMissingCacheException( 'Missing cache module' );
+  }
+
+}
+
+
+// Exceptions
+class CacheableMissingCacheException extends Exception {}
