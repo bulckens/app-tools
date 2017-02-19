ed9a4857e227f6ad4235497fac755aee29b8e650
wout <wout@impinc.co.uk>
2017-02-19 17:35:24 +0100

    Added cache module

---
+++ spec/Bulckens/AppTools/CacheSpec.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace spec\Bulckens\AppTools;
+
+use Bulckens\AppTools\Cache;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+use Bulckens\AppTools\App;
+
+class CacheSpec extends ObjectBehavior {
+
+  function let() {
+    $app = new App( 'dev' );
+    $app->run();
+  }
+
+  function letGo() {
+    $this->delete( 'monkey' );
+    $this->delete( 'monkey.loves' );
+    $this->delete( 'monkey.hates' );
+  }
+
+
+  // Set method
+  function it_stores_a_given_value() {
+    $this->get( 'monkey' )->shouldBe( null );
+    $this->set( 'monkey', 'banana' );
+    $this->get( 'monkey' )->shouldBe( 'banana' );
+  }
+
+  function it_returns_itself_after_storing_a_given_value() {
+    $this->set( 'monkey', 'banana' )->shouldBe( $this );
+  }
+
+
+  // Get method
+  function it_returns_a_stored_value() {
+    $this->set( 'monkey', 'banana' );
+    $this->get( 'monkey' )->shouldBe( 'banana' );
+  }
+
+
+  // Delete method
+  function it_deletes_a_given_key() {
+    $this->set( 'monkey.loves', 'banana' );
+    $this->delete( 'monkey.loves' );
+    $this->get( 'monkey.loves' )->shouldBe( null );
+  }
+
+  function it_returns_itself_after_deleting_a_given_key() {
+    $this->delete( 'monkey.loves' )->shouldBe( $this );
+  }
+
+
+  // Has method
+  function it_checks_the_existence_of_a_given_key() {
+    $this->has( 'monkey.hates' )->shouldBe( false );
+    $this->set( 'monkey.hates', 'pickles' );
+    $this->has( 'monkey.hates' )->shouldBe( true );
+  }
+
+    
+  // Config method
+  function it_returns_the_config_instance_without_an_argument() {
+    $this->config()->shouldHaveType( 'Bulckens\AppTools\Config' );
+  }
+
+  function it_returns_the_the_value_for_a_given_key() {
+    $this->config( 'engine' )->shouldBe( 'redis' );
+  }
+
+  function it_returns_a_given_default_value_if_key_is_not_existing() {
+    $this->config( 'pater', 'nostrum' )->shouldBe( 'nostrum' );
+  }
+
+
+  // File method
+  function it_builds_config_file_name_from_class() {
+    $this->file()->shouldBe( 'cache.yml' );
+  }
+
+  function it_defines_a_custom_config_file() {
+    $this->file( 'cache_custom.yml' );
+    $this->file()->shouldBe( 'cache_custom.yml' );
+    $this->config( 'engine' )->shouldBe( 'custom' );
+  }
+
+  function it_unsets_the_custom_config_file_with_null_given() {
+    $this->file( 'cache_custom.yml' );
+    $this->file()->shouldBe( 'cache_custom.yml' );
+    $this->file( null );
+    $this->file()->shouldBe( 'cache.yml' );
+  }
+
+  function it_returns_itself_after_defining_a_custom_config_file() {
+    $this->file( 'cache_custom.yml' )->shouldBe( $this );
+  }
+
+}
---
+++ src/Bulckens/AppTools/Cache.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Bulckens\AppTools;
+
+use Desarrolla2\Cache\Cache as Desarrolla2;
+use Desarrolla2\Cache\Adapter\File;
+use Desarrolla2\Cache\Adapter\Predis;
+use Desarrolla2\Cache\Adapter\NotCache;
+use Desarrolla2\Cache\Adapter\Memcache;
+use Bulckens\AppTools\Traits\Configurable;
+
+class Cache {
+
+  use Configurable;
+
+  protected $cache;
+  protected $lifespan;
+  protected $prefix;
+  protected $delimiter = '.';
+
+  public function __construct() {
+    // set default lifespan
+    $this->lifespan = $this->config( 'lifespan', 60 * 60 * 24 * 30 );
+
+    // set prefix
+    $this->prefix = $this->config( 'prefix', 'bulckens.app_tools.cache' );
+
+    // initialize adaptor
+    switch ( $this->config( 'engine' ) ) {
+      case 'redis':
+        $adapter = new Predis();
+        $adapter->setOption( 'ttl', $this->lifespan );
+        $this->delimiter = ':';
+      break;
+      case 'file':
+        $dir = App::root( $this->config( 'dir', 'tmp/cache' ) );
+        $adapter = new File( $dir );
+        $adapter->setOption( 'ttl', $this->lifespan );
+      break;
+      case 'memcache':
+        $adapter = new Memcache();
+      break;
+      default:
+        $adapter = new NotCache();
+      break;
+    }
+
+    // initialize cache
+    $this->cache = new Desarrolla2( $adapter );
+  }
+
+
+  // Create item
+  public function set( $key, $value, $lifespan = null ) {
+    $this->cache->set( $this->prefix( $key ), $value, $lifespan ?: $this->lifespan );
+
+    return $this;
+  }
+
+
+  // Read item
+  public function get( $key ) {
+    if ( $value = $this->cache->get( $this->prefix( $key ) ) )
+      return $value;
+  }
+
+
+  // Delete item
+  public function delete( $key ) {
+    $this->cache->delete( $this->prefix( $key ) );
+
+    return $this;
+  }
+
+
+  // Test if item is present
+  public function has( $key ) {
+    return !! $this->cache->has( $this->prefix( $key ) );
+  }
+
+
+  // Prefix given key
+  protected function prefix( $key ) {
+    return str_replace( '.', $this->delimiter, $this->prefix . "$this->delimiter$key" );
+  }
+
+}
