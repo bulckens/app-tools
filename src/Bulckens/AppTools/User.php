ad19a01f54e4203f4659eb7a96167ad60f7d5104
wout <wout@mick-wout.com>
2017-04-09 21:07:28 +0200

    Extended User class and added password reset interface

--- src/Bulckens/AppTools/User.php
+++ src/Bulckens/AppTools/User.php
@@ -11,6 +11,8 @@ class User {
 
   use Configurable;
 
+  protected static $activate_at_login = false;
+
   // Initialize database connection
   public function __construct() {
     // setup a new Eloquent Capsule instance
@@ -27,20 +29,37 @@ class User {
     ]);
 
     $capsule->bootEloquent();
+
+    // setup chckpoints checkpoints
+    Sentinel::enableCheckpoints();
+
+    // configure activation checkpoint
+    if ( in_array( 'activation', $this->config( 'checkpoints' ) ) )
+      self::$activate_at_login = $this->config( 'login.activate', false );
+    else
+      Sentinel::removeCheckpoint( 'activation' );
+
+    // configure activation checkpoint
+    if ( ! in_array( 'throttle', $this->config( 'checkpoints' ) ) )
+      Sentinel::removeCheckpoint( 'throttle' );
   }
 
 
   // Register user
-  public function register( $email, $pass ) {
-    return Sentinel::register([
+  public static function register( $email, $pass, $activate = false ) {
+    $credentials = [
       'email'    => $email
     , 'password' => $pass
-    ]);
+    ];
+
+    return $activate === true ?
+      Sentinel::registerAndActivate( $credentials ) :
+      Sentinel::register( $credentials );
   }
 
 
-  // Authenticate user
-  public function auth( $email, $pass, $remember = false ) {
+  // Log user in
+  public static function login( $email, $pass, $remember = false ) {
     return Sentinel::authenticate([
       'email'    => $email
     , 'password' => $pass
@@ -48,15 +67,85 @@ class User {
   }
 
 
+  // log user out
+  public static function logout() {
+    return Sentinel::logout();
+  }
+
+
   // Checks if user is logged in
-  public function loggedIn() {
+  public static function loggedIn() {
     return Sentinel::check();
   }
 
 
-  // Get user instance
-  public function get() {
+  // Get the instance of the current logged in user
+  public static function get() {
     return Sentinel::getUser();
   }
 
+
+  // Find user by email or id
+  public static function find( $id ) {
+    if ( is_numeric( $id ) )
+      return Sentinel::findById( $id );
+
+    return Sentinel::findByCredentials([ 'login' => $id ]);
+  }
+
+
+  // Generate password reset link
+  public static function resetCode( $email ) {
+    if ( $user = self::find( $email ) ) {
+      $reminder = Sentinel::getReminderRepository()->create( $user );
+      return $reminder->code;
+
+    } else {
+      throw new UserNotFoundException( "Unable to find user for email $email" );
+    }
+  }
+
+
+  // Reset password using code
+  public function resetPassword( $email, $password, $code ) {
+    if ( $user = self::find( $email ) ) {
+      // get reminder repo
+      $reminder = Sentinel::getReminderRepository();
+
+      // test existance
+      if ( $reminder->exists( $user, $code ) )
+        return $reminder->complete( $user, $code, $password );
+
+      throw new UserResetCodeNotValidException( "The password reset code $code is not or no longer valid" );
+
+    } else {
+      throw new UserNotFoundException( "Unable to find user for email $email" );
+    }
+  }
+
 }
+
+
+// Exceptions
+class UserNotFoundException extends Exception {}
+class UserResetCodeNotValidException extends Exception {}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
