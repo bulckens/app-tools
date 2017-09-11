9884b29ccbbebea150de355825f796c385ac3e2e
wout <wout@mick-wout.com>
2017-09-11 20:12:34 +0200

    Added the possibility to make a columns unchangeable in validations

--- src/Bulckens/AppTests/TestModel.php
+++ src/Bulckens/AppTests/TestModel.php
@@ -4,4 +4,14 @@ namespace Bulckens\AppTests;
 
 use Bulckens\AppTools\Model;
 
-class TestModel extends Model {}
+class TestModel extends Model {
+
+  public function parents() {
+    return $this->belongsTo( 'Bulckens\AppTests\TestModel' );
+  }
+
+  public function children() {
+    return $this->hasMany( 'Bulckens\AppTests\TestModel', 'parent_id' );
+  }
+
+}
