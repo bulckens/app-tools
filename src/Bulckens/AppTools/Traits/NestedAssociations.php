ca498d8476360601eba9c474ec18fa2cefbc608a
wout <wout@mick-wout.com>
2017-09-16 09:45:12 +0200

    Allowing save options to be passed

--- src/Bulckens/AppTools/Traits/NestedAssociations.php
+++ src/Bulckens/AppTools/Traits/NestedAssociations.php
@@ -79,8 +79,8 @@ trait NestedAssociations {
 
 
   // Saves itself and the nested associations
-  public function saveWithNestedAssociations() {
-    if ( $save = parent::save() ) {
+  public function saveWithNestedAssociations( array $options = [] ) {
+    if ( $save = parent::save( $options ) ) {
       $this->saveNestedAssociations();
     }
 
