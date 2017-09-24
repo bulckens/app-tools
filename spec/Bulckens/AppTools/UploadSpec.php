3841f39976e46a0ea888d2d07a1d402f53403fcf
wout <wout@mick-wout.com>
2017-09-24 19:50:26 +0200

    Fully functional upload handler with filesystem, stream and s3 support

--- spec/Bulckens/AppTools/UploadSpec.php
+++ spec/Bulckens/AppTools/UploadSpec.php
@@ -13,6 +13,9 @@ class UploadSpec extends ObjectBehavior {
   
   function let() {
     global $_FILES;
+    global $_SERVER;
+
+    $_SERVER['HTTP_HOST'] = 'localhost';
 
     $app = new App( 'dev' );
     $app->run();
@@ -31,7 +34,7 @@ class UploadSpec extends ObjectBehavior {
     exec( sprintf( 'rm -rf %s', escapeshellarg( App::root( 'dev/upload/test' ) ) ) );
   }
 
-  
+
   // Initialization
   function it_stores_the_key() {
     $this->key()->shouldBe( 'image' );
@@ -197,13 +200,13 @@ class UploadSpec extends ObjectBehavior {
 
   // Size method
   function it_returns_the_size() {
-    $this->size()->shouldBe( 118603 );
+    $this->size()->shouldBe( 18338 );
   }
 
 
   // Weight method (human readable size)
   function it_returns_the_weight() {
-    $this->weight()->shouldBe( '115.82 KB' );
+    $this->weight()->shouldBe( '17.91 KB' );
   }
 
 
@@ -240,26 +243,79 @@ class UploadSpec extends ObjectBehavior {
 
 
   // Store method
-  function it_stores_the_file_locally() {
+  function it_stores_the_file_on_the_file_system() {
     $this->store()->shouldBe( true );
+    $this->url()->shouldBe( 'https://localhost/dev/upload/test/w.jpg' );
   }
 
-  function it_stores_the_file_locally_into_a_given_directory() {
-    $this->store( 'mecaniq/arms' )->shouldBe( true );
+  function it_stores_the_file_on_the_file_system_into_a_given_directory() {
+    $this->dir( 'mecaniq/arms' )->store()->shouldBe( true );
+    $this->path()->shouldBe( '/dev/upload/test/mecaniq/arms/w.jpg' );
   }
 
   function it_stores_the_file_on_s3() {
-
+    $this->beConstructedWith( 'image', [
+      'storage' => 's3'
+    ]);
+    $this->store()->shouldBe( true );
+    $this->url()->shouldBe( 'https://zow-v5-test.s3-eu-central-1.amazonaws.com/w.jpg' );
   }
 
   function it_stores_the_file_on_s3_into_a_given_directory() {
-    
+    $this->beConstructedWith( 'image', [
+      'storage' => 's3'
+    ]);
+    $this->dir( 'some/other/dir' )->store()->shouldBe( true );
+    $this->url()->shouldEndWith( 'https://zow-v5-test.s3-eu-central-1.amazonaws.com/some/other/dir/w.jpg' );
+  }
+
+  function it_stores_the_file_on_s3_into_a_given_bucket() {
+    $this->beConstructedWith( 'image', [
+      'storage' => 's3_ireland'
+    ]);
+    $this->store()->shouldBe( true );
+    $this->url()->shouldEndWith( 'https://zow-v5-test-alternative.s3-eu-west-1.amazonaws.com/w.jpg' );
   }
 
   function it_fails_if_the_given_storage_type_does_not_exist() {
     $this->beConstructedWith( 'image', [ 'config' => 'upload_unknown.yml' ]);
     $this->shouldThrow( 'Bulckens\AppTools\UploadStorageTypeUnknownException' )->duringStore();
   }
+
+  function it_fails_when_store_is_called_twice() {
+    $this->store()->shouldBe( true );
+    $this->shouldThrow( 'Bulckens\AppTools\UploadAlreadyStoredException' )->duringStore();
+  }
+
+  function it_fails_when_no_access_key_and_or_secret_are_configured() {
+    $this->beConstructedWith( 'image', [
+      'config' => 'upload_credentialless.yml'
+    ]);
+    $this->shouldThrow( 'Bulckens\AppTools\UploadS3CredentialsNotDefinedException' )->duringStore();
+  }
+  
+  function it_fails_when_no_region_is_defined() {
+    $this->beConstructedWith( 'image', [
+      'config' => 'upload_regionless.yml'
+    ]);
+    $this->shouldThrow( 'Bulckens\AppTools\UploadS3RegionNotDefinedException' )->duringStore();
+  }
+
+  function it_fails_when_no_bucket_is_defined() {
+    $this->beConstructedWith( 'image', [
+      'config' => 'upload_bucketless.yml'
+    ]);
+    $this->shouldThrow( 'Bulckens\AppTools\UploadS3BucketNotDefinedException' )->duringStore();
+  }
+
+  function it_fails_when_the_bucket_does_not_exist() {
+    $this->beConstructedWith( 'image', [
+      'config' => 'upload_unknown.yml'
+    , 'storage' => 's3'
+    ]);
+    $this->shouldThrow( 'Aws\S3\Exception\S3Exception' )->duringStore();
+  }
+
 
 
   // File method
@@ -279,7 +335,8 @@ class UploadSpec extends ObjectBehavior {
   function it_returns_the_full_destination_file_path_with_an_additional_sub_path() {
     $this->name( 'substored' );
     $this
-      ->file( 'picture/nicely' )
+      ->dir( 'picture/nicely' )
+      ->file()
       ->shouldStartWith( App::root( 'dev/upload/test/picture/nicely/substored.jpg' ) );
   }
 
@@ -288,10 +345,84 @@ class UploadSpec extends ObjectBehavior {
     $this->file()->shouldMatch( '/w\.\d{13}\.jpg$/' );
   }
 
+  function it_returns_the_absolute_path_for_filesystem_storage() {
+    $this->dir( 'will/power' )->file()->shouldBe( App::root( 'dev/upload/test/will/power/w.jpg' ) );
+  }
 
-  // Multiple static method
-  function it_allows_multiple_files_to_be_uploaded() {
+  function it_returns_the_absolute_path_for_filesystem_streamed_storage() {
+    $this->beConstructedWith( 'image', [
+      'storage' => 'external'
+    ]);
+    $this->dir( 'will/power' )->file()->shouldBe( "http://server.local/safe/to/store/will/power/w.jpg" );
+  }
 
+  function it_returns_the_relative_path_for_s3_storage() {
+    $this->beConstructedWith( 'image', [
+      'storage' => 's3'
+    ]);
+    $this->dir( 'will/power' )->file()->shouldBe( '/will/power/w.jpg' );
+  }
+
+
+  // Dir method
+  function it_returns_the_dir() {
+    $this->dir( 'halla/23/malla' );
+    $this->dir()->shouldBe( 'halla/23/malla' );
+  }
+
+  function it_sets_the_dir_of_the_file() {
+    $this->dir()->shouldBe( null );
+    $this->dir( 'some/sub/directory' );
+    $this->dir()->shouldBe( 'some/sub/directory' );
+  }
+
+  function it_strips_any_leading_and_trailing_slashes() {
+    $this->dir( '/some/sub/directory/' );
+    $this->dir()->shouldBe( 'some/sub/directory' );
+  }
+
+  function it_returns_itself_after_setting_the_path_of_the_file() {
+    $this->dir( 'some/sub/directory' )->shouldBe( $this );
+  }
+
+
+  // Path method
+  function it_returns_the_public_path_to_the_file() {
+    $this->path()->shouldBe( '/dev/upload/test/w.jpg' );
+  }
+
+  function it_returns_the_public_s3_path_to_the_file() {
+    $this->beConstructedWith( 'image', [
+      'storage' => 's3'
+    ]);
+    $this->path()->shouldBe( '/w.jpg' );
+  }
+
+
+  // Url method
+  function it_returns_the_url_of_the_file() {
+    $this->url()->shouldBe( 'https://localhost/dev/upload/test/w.jpg' );
+  }
+
+  function it_returns_the_url_of_the_file_with_a_cofigured_host() {
+    $this->beConstructedWith( 'image', [
+      'config' => 'upload_with_host.yml'
+    ]);
+    $this->url()->shouldBe( 'https://superserver.com/custom/upload/test/w.jpg' );
+  }
+
+  function it_returns_the_url_of_the_file_with_a_cofigured_host_and_a_custom_protocol() {
+    $this->beConstructedWith( 'image', [
+      'config' => 'upload_with_host.yml'
+    ]);
+    $this->url( 'http:' )->shouldBe( 'http://superserver.com/custom/upload/test/w.jpg' );
+  }
+
+  function it_returns_the_s3_url_of_the_file() {
+    $this->beConstructedWith( 'image', [
+      'storage' => 's3'
+    ]);
+    $this->url()->shouldBe( 'https://zow-v5-test.s3-eu-central-1.amazonaws.com/w.jpg' ); 
   }
 
 
