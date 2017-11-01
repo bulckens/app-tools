61e3b7a90dc75f9c2fc2760d6f1aff8508592102
wout <wout@mick-wout.com>
2017-11-01 14:33:56 +0100

    Upload styles with image conversions

--- spec/Bulckens/AppTools/UploadSpec.php
+++ spec/Bulckens/AppTools/UploadSpec.php
@@ -105,7 +105,7 @@ class UploadSpec extends ObjectBehavior {
 
   function it_fails_if_the_given_upload_is_not_a_string_or_an_array() {
     $this
-      ->shouldThrow( 'Bulckens\AppTools\UploadSourceNotAccptableException' )
+      ->shouldThrow( 'Bulckens\AppTools\UploadSourceNotAcceptableException' )
       ->during__construct( 123 );
   }
 
@@ -221,15 +221,6 @@ class UploadSpec extends ObjectBehavior {
     $this->name()->shouldEndWith( 'DOG FØØD CONTAINER≈.jpg' );
   }
 
-  function it_sets_a_custom_name() {
-    $this->name( 'beast' );
-    $this->name()->shouldEndWith( 'beast.jpg' );
-  }
-
-  function it_returns_itself_after_setting_the_name() {
-    $this->name( 'coolaid.jpg' )->shouldBe( $this );
-  }
-
   function it_returns_the_file_name_formatted_as_configured_globally() {
     $_FILES['image'] = [
       'name' => 'DOG FØØD CONTAINER≈.JPG'
@@ -240,7 +231,7 @@ class UploadSpec extends ObjectBehavior {
       'config' => 'upload_formatted.yml'
     , 'styles' => [ 'large' => '2560x2560#' ]
     ]);
-    $this->name([ 'style' => 'large' ])->shouldEndWith( 'dog-food-container-large.jpg' );
+    $this->name( 'large' )->shouldEndWith( 'dog-food-container-large.jpg' );
   }
 
   function it_returns_the_file_name_formatted_as_configured_locally() {
@@ -254,7 +245,7 @@ class UploadSpec extends ObjectBehavior {
     , 'name' => '{{ style }}.{{ name }}'
     , 'styles' => [ 'medium' => '1280x1280#' ]
     ]);
-    $this->name([ 'style' => 'medium' ])->shouldEndWith( 'medium.dog-food-container.jpg' );
+    $this->name( 'medium' )->shouldEndWith( 'medium.dog-food-container.jpg' );
   }
 
   function it_returns_the_file_name_formatted_as_configured_locally_with_width_and_height() {
@@ -268,7 +259,67 @@ class UploadSpec extends ObjectBehavior {
     , 'name' => '{{ style }}-{{ width }}x{{ height }}.{{ name }}'
     , 'styles' => [ 'medium' => '1280x1280#' ]
     ]);
-    $this->name([ 'style' => 'medium' ])->shouldEndWith( 'medium-320x320.dog-food-container.jpg' );
+    $this->name( 'medium' )->shouldEndWith( 'medium-320x320.dog-food-container.jpg' );
+  }
+
+  function it_returns_the_file_name_formatted_as_configured_locally_with_a_default_style_label() {
+    $_FILES['image'] = [
+      'name' => 'DOG FØØD CONTAINER≈.JPG'
+    , 'tmp_name' => self::setupTmpFile()
+    , 'error' => UPLOAD_ERR_OK
+    ];
+    $this->beConstructedWith( 'image', [
+      'config' => 'upload_formatted.yml'
+    , 'name' => 'image-{{ style }}.{{ name }}'
+    ]);
+    $this->name()->shouldEndWith( 'image-original.dog-food-container.jpg' );
+  }
+
+  function it_uses_the_default_name_format_if_styles_are_defined_but_no_format_is_given() {
+    $_FILES['image'] = [
+      'name' => 'DOG FØØD CONTAINER≈.JPG'
+    , 'tmp_name' => self::setupTmpFile()
+    , 'error' => UPLOAD_ERR_OK
+    ];
+    $this->beConstructedWith( 'image', [
+      'styles' => [
+        'mini' => '10x10!'
+      , 'original' => '1024x1024>'
+      ]
+    ]);
+    $this->name( 'mini' )->shouldEndWith( 'dog-food-container-mini.jpg' );
+    $this->name( 'original' )->shouldEndWith( 'dog-food-container-original.jpg' );
+  }
+
+  function it_uses_the_original_style_when_no_style_is_given_but_styles_are_defined() {
+    $_FILES['image'] = [
+      'name' => 'DOG FØØD CONTAINER≈.JPG'
+    , 'tmp_name' => self::setupTmpFile()
+    , 'error' => UPLOAD_ERR_OK
+    ];
+    $this->beConstructedWith( 'image', [
+      'styles' => [
+        'mini' => '10x10!'
+      , 'original' => '1024x1024>'
+      ]
+    ]);
+    $this->name()->shouldEndWith( 'dog-food-container-original.jpg' );
+  }
+
+
+  // Rename method
+  function it_sets_a_custom_name() {
+    $this->rename( 'beast' );
+    $this->name()->shouldEndWith( 'beast.jpg' );
+  }
+
+  function it_sets_a_custom_name_but_ignores_a_given_extension() {
+    $this->rename( 'coolaid.gif' );
+    $this->name()->shouldEndWith( 'coolaid.jpg' );
+  }
+
+  function it_returns_itself_after_setting_the_name() {
+    $this->rename( 'coolaid.jpg' )->shouldBe( $this );
   }
 
 
@@ -279,12 +330,22 @@ class UploadSpec extends ObjectBehavior {
 
   function it_returns_the_real_extension_even_if_none_is_given() {
     $_FILES['image'] = [
-      'name' => 'DOGFOOD_CONTAINER.JPG'
+      'name' => 'DOGFOOD_CONTAINER'
     , 'tmp_name' => self::setupTmpFile()
     , 'error' => UPLOAD_ERR_OK
     ];
     $this->beConstructedWith( 'image' );
     $this->ext()->shouldBe( 'jpg' );
+  }
+
+  function it_returns_the_real_extension_even_if_the_wrong_one_is_given() {
+    $_FILES['image'] = [
+      'name' => 'fake.jpg'
+    , 'tmp_name' => self::setupTmpFile( 'fake.jpg' )
+    , 'error' => UPLOAD_ERR_OK
+    ];
+    $this->beConstructedWith( 'image' );
+    $this->ext()->shouldBe( 'txt' );
   }
 
 
@@ -293,6 +354,147 @@ class UploadSpec extends ObjectBehavior {
     $root = str_replace( '/', '\/', App::root( 'dev/upload/tmp/[A-Za-z0-9]{32}' ) );
     $this->tmpName()->shouldMatch( "/$root/" );
   }
+
+  function it_returns_the_tmp_name_for_a_style() {
+    $this->beConstructedWith( 'image', [
+      'styles' => [
+        'medium' => '1024x1024#'
+      ]
+    ]);
+    $root = str_replace( '/', '\/', App::root( 'dev/upload/tmp/[A-Za-z0-9]{32}\.style-medium' ) );
+    $this->tmpName( 'medium' )->shouldMatch( "/$root/" );
+  }
+
+  function it_creates_the_tmp_file_for_a_style() {
+    $this->beConstructedWith( 'image', [
+      'styles' => [
+        'mastaba' => '1024x1024#'
+      ]
+    ]);
+    $tmp_name = $this->tmpName( 'mastaba' )->getWrappedObject();
+
+    if ( ! file_exists( $tmp_name ) ) {
+      throw new Exception( "File $tmp_name does not exist" );
+    }
+  }
+
+  function it_converts_the_image_to_be_cropped_to_given_dimensions_in_the_style() {
+    $_FILES['image'] = [ 'name' => 'm.jpg', 'tmp_name' => self::setupTmpFile( 'm.jpg' ), 'error' => UPLOAD_ERR_OK ];
+
+    $this->beConstructedWith( 'image', ['styles' => [ 'small' => '128x128#' ] ]);
+    $tmp_name = $this->tmpName( 'small' )->getWrappedObject();
+    $size = getimagesize( $tmp_name );
+
+    if ( $size[0] != 128 || $size[1] != 128 ) {
+      throw new Exception( "Expected the file to be 128x128px wide but got {$size[0]}x{$size[1]}px" );
+    }
+  }
+
+  function it_converts_the_image_to_fit_a_given_box_thereby_cropping_the_image() {
+    $_FILES['image'] = [ 'name' => 'm.jpg', 'tmp_name' => self::setupTmpFile( 'm.jpg' ), 'error' => UPLOAD_ERR_OK ];
+
+    $this->beConstructedWith( 'image', [ 'styles' => [ 'small' => '256x256' ] ]);
+    $tmp_name = $this->tmpName( 'small' )->getWrappedObject();
+    $size = getimagesize( $tmp_name );
+
+    if ( $size[0] != 177 || $size[1] != 256 ) {
+      throw new Exception( "Expected the file fit within 256x256px but got {$size[0]}x{$size[1]}px" );
+    }
+  }
+
+  function it_converts_the_image_to_fit_a_given_box_and_scales_the_image_up_if_it_is_too_small_while_respecting_the_aspect_ratio() {
+    $_FILES['image'] = [ 'name' => 'm.jpg', 'tmp_name' => self::setupTmpFile( 'm.jpg' ), 'error' => UPLOAD_ERR_OK ];
+
+    $this->beConstructedWith( 'image', [ 'styles' => [ 'small' => '2560x2560' ] ]);
+    $tmp_name = $this->tmpName( 'small' )->getWrappedObject();
+    $size = getimagesize( $tmp_name );
+
+    if ( $size[0] != 1768 || $size[1] != 2560 ) {
+      throw new Exception( "Expected the file fit within 2560x2560px but got {$size[0]}x{$size[1]}px" );
+    }
+  }
+
+  function it_converts_the_image_to_fit_a_given_box_without_scaling_the_image_up_if_it_is_too_small() {
+    $_FILES['image'] = [ 'name' => 'm.jpg', 'tmp_name' => self::setupTmpFile( 'm.jpg' ), 'error' => UPLOAD_ERR_OK ];
+
+    $this->beConstructedWith( 'image', [ 'styles' => [ 'small' => '2560x2560>' ] ]);
+    $tmp_name = $this->tmpName( 'small' )->getWrappedObject();
+    $size = getimagesize( $tmp_name );
+
+    if ( $size[0] != 746 || $size[1] != 1080 ) {
+      throw new Exception( "Expected the file fit within 2560x2560px without scaling it up but got {$size[0]}x{$size[1]}px" );
+    }
+  }
+
+  function it_converts_the_image_to_fit_a_given_box_and_scales_the_image_up_if_it_is_too_small_while_not_respecting_the_aspect_ratio() {
+    $_FILES['image'] = [ 'name' => 'm.jpg', 'tmp_name' => self::setupTmpFile( 'm.jpg' ), 'error' => UPLOAD_ERR_OK ];
+
+    $this->beConstructedWith( 'image', [ 'styles' => [ 'small' => '2560x2560!' ] ]);
+    $tmp_name = $this->tmpName( 'small' )->getWrappedObject();
+    $size = getimagesize( $tmp_name );
+
+    if ( $size[0] != 2560 || $size[1] != 2560 ) {
+      throw new Exception( "Expected the image to be exactly 2560x2560px but got {$size[0]}x{$size[1]}px" );
+    }
+  }
+
+  function it_converts_the_image_with_an_additional_image_magick_command() {
+    // NOTE: this should be verified manually
+    $this->beConstructedWith( 'image', [
+      'styles' => [
+        'small' => '230x230#'
+      ]
+    , 'convert' => '-colorspace Gray'
+    ]);
+    $this->tmpName( 'small' )->shouldBeString();
+  }
+
+  function it_converts_the_image_with_an_additional_image_magick_command_for_every_style() {
+    // NOTE: this should be verified manually
+    $this->beConstructedWith( 'image', [
+      'styles' => [
+        'small' => '240x240#'
+      , 'medium' => '300x350#'
+      , 'original' => '640x640>'
+      ]
+    , 'convert' => [
+        'small' => '-colorspace Gray'
+      , 'medium' => '\( +clone -sepia-tone 60% \) -average'
+      ]
+    ]);
+    $this->tmpName( 'small' )->shouldBeString();
+    $this->tmpName( 'medium' )->shouldBeString();
+    $this->tmpName( 'original' )->shouldBeString();
+  }
+
+  function it_fails_if_the_resize_command_for_the_style_is_invalid() {
+    $this->beConstructedWith( 'image', [
+      'styles' => [
+        'mini' => '128x 128#'
+      ]
+    ]);
+    $this->shouldThrow( 'Bulckens\AppTools\UploadStyleNotValidException' )->duringTmpName( 'mini' );
+  }
+
+  function it_fails_if_the_resize_command_for_the_style_is_defines_no_value_at_all() {
+    $this->beConstructedWith( 'image', [
+      'styles' => [
+        'mini' => 'x>'
+      ]
+    ]);
+    $this->shouldThrow( 'Bulckens\AppTools\UploadStyleNotValidException' )->duringTmpName( 'mini' );
+  }
+
+  function it_fails_when_the_image_magick_command_could_not_be_found() {
+    $this->beConstructedWith( 'image', [
+      'styles' => [
+        'massive' => '1001x1001#'
+      ]
+    , 'config' => 'upload_bad_magick.yml'
+    ]);
+    $this->shouldThrow( 'Bulckens\AppTools\UploadImageMagickNotFoundException' )->duringTmpName( 'massive' );
+  }
+
 
 
   // Error method
@@ -417,6 +619,19 @@ class UploadSpec extends ObjectBehavior {
     $this->url()->shouldBe( 'https://zow-v5-test.s3-eu-central-1.amazonaws.com/w.jpg' );
   }
 
+  function it_stores_all_the_styles_on_s3() {
+    $this->beConstructedWith( 'image', [
+      'storage' => 's3'
+    , 'styles' => [
+        'mini' => '64x64#'
+      , 'small' => '128x128#'
+      , 'original' => '1028x1028>'
+      ]
+    ]);
+    $this->dir( 'with_styles' )->store()->shouldBe( true );
+    $this->url( 'original' )->shouldEndWith( 'https://zow-v5-test.s3-eu-central-1.amazonaws.com/with_styles/w-original.jpg' );
+  }
+
   function it_stores_the_file_on_s3_into_a_given_directory() {
     $this->beConstructedWith( 'image', [
       'storage' => 's3'
@@ -481,15 +696,28 @@ class UploadSpec extends ObjectBehavior {
       ->shouldStartWith( App::root( 'dev/upload/test/w.jpg' ) );
   }
 
+  function it_returns_the_full_destination_file_path_with_a_style() {
+    $this->beConstructedWith( 'image', [
+      'styles' => [
+        'micro' => '2x2#'
+      , 'original' => '1280x1280>'
+      ]
+    ]);
+
+    $this
+      ->file( 'micro' )
+      ->shouldStartWith( App::root( 'dev/upload/test/w-micro.jpg' ) );
+  }
+
   function it_returns_the_full_destination_file_path_with_a_custom_name() {
-    $this->name( 'stored' );
+    $this->rename( 'stored' );
     $this
       ->file()
       ->shouldStartWith( App::root( 'dev/upload/test/stored.jpg' ) );
   }
 
   function it_returns_the_full_destination_file_path_with_an_additional_sub_path() {
-    $this->name( 'substored' );
+    $this->rename( 'substored' );
     $this
       ->dir( 'picture/nicely' )
       ->file()
@@ -547,6 +775,15 @@ class UploadSpec extends ObjectBehavior {
     $this->path()->shouldBe( '/dev/upload/test/w.jpg' );
   }
 
+  function it_returns_the_public_path_to_the_file_for_a_given_style() {
+    $this->beConstructedWith( 'image', [
+      'styles' => [
+        'mini' => '256x256#'
+      ]
+    ]);
+    $this->path( 'mini' )->shouldEndWith( '/dev/upload/test/w-mini.jpg' );
+  }
+
   function it_returns_the_public_s3_path_to_the_file() {
     $this->beConstructedWith( 'image', [
       'storage' => 's3'
@@ -554,10 +791,39 @@ class UploadSpec extends ObjectBehavior {
     $this->path()->shouldBe( '/w.jpg' );
   }
 
+  function it_returns_the_public_s3_path_to_the_file_for_a_given_style() {
+    $this->beConstructedWith( 'image', [
+      'storage' => 's3'
+    , 'styles' => [
+        'tiny' => '128x128#'
+      ]
+    ]);
+    $this->path( 'tiny' )->shouldBe( '/w-tiny.jpg' );
+  }
+
 
   // Url method
   function it_returns_the_url_of_the_file() {
     $this->url()->shouldBe( 'https://localhost/dev/upload/test/w.jpg' );
+  }
+
+  function it_returns_the_url_of_the_file_in_a_given_style() {
+    $this->beConstructedWith( 'image', [
+      'styles' => [
+        'frop' => '21x21#'
+      ]
+    ]);
+    $this->url( 'frop' )->shouldBe( 'https://localhost/dev/upload/test/w-frop.jpg' );
+  }
+
+  function it_returns_the_url_of_the_file_in_the_original_style_by_default() {
+    $this->beConstructedWith( 'image', [
+      'styles' => [
+        'frop' => '21x21#'
+      , 'original' => '300x300'
+      ]
+    ]);
+    $this->url()->shouldEndWith( 'https://localhost/dev/upload/test/w-original.jpg' );
   }
 
   function it_returns_the_url_of_the_file_with_a_cofigured_host() {
@@ -571,7 +837,7 @@ class UploadSpec extends ObjectBehavior {
     $this->beConstructedWith( 'image', [
       'config' => 'upload_with_host.yml'
     ]);
-    $this->url( 'http:' )->shouldBe( 'http://superserver.com/custom/upload/test/w.jpg' );
+    $this->url( 'original', [ 'protocol' => 'http:' ] )->shouldBe( 'http://superserver.com/custom/upload/test/w.jpg' );
   }
 
   function it_returns_the_s3_url_of_the_file() {
