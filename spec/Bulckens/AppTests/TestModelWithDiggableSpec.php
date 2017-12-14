a8d48585c2b6ac521ab8db26f0c809124908450e
wout <wout@mick-wout.com>
2017-12-14 18:31:36 +0100

    Forced fallback value in diggable trait

---
+++ spec/Bulckens/AppTests/TestModelWithDiggableSpec.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace spec\Bulckens\AppTests;
+
+use Bulckens\AppTools\App;
+use Bulckens\AppTests\TestModelWithDiggable;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class TestModelWithDiggableSpec extends ObjectBehavior {
+
+  function let() {
+    $app = new App( 'dev' );
+    $app->run();
+  }
+  
+  // Get method
+  function it_gets_a_root_key() {
+    $this->get( 'apparatus' )->shouldBe( 'time travel machine' );
+  }
+
+  function it_gets_a_nested_key() {
+    $this->get( 'first.second.apparatus' )->shouldBe( 'felange' );
+  }
+
+  function it_defaults_to_a_given_value_if_the_key_could_not_be_found() {
+    $this->get( 'first.second.third.apparatus', 'gyrocar' )->shouldBe( 'gyrocar' );
+  }
+
+  function it_returns_the_key_if_explicitly_required() {
+    $this->get( 'first.second.third.apparatus', null, true )->shouldBe( 'first.second.third.apparatus' );
+  }
+
+}
