c74e58ca7f3b21d7fa276115f183e2625bdc8bf7
wout <wout@mick-wout.com>
2018-01-23 16:51:03 +0100

    Allowing custom validations in UserAuth

--- src/Bulckens/AppTools/UserAuth.php
+++ src/Bulckens/AppTools/UserAuth.php
@@ -5,6 +5,8 @@ namespace Bulckens\AppTools;
 class UserAuth {
   
   protected $permission;
+  protected $output;
+  protected $user;
 
   public function __construct( $permission = null ) {
     $this->permission = $permission;
@@ -18,41 +20,48 @@ class UserAuth {
     $format = pathinfo( $uri, PATHINFO_EXTENSION );
 
     // initialize output container
-    $output = new Output( empty( $format ) ? 'html' : $format );
+    $this->output = new Output( empty( $format ) ? 'html' : $format );
     
     // fail when not being logged in
     if ( ! User::loggedIn( $req->getParam( 'user_token' ) ) ) {
-      $output->add([ 'error' => 'login.required' ])->status( 401 );
+      $this->output->add([ 'error' => 'login.required' ])->status( 401 );
 
-    } elseif ( ! is_null( $this->permission ) ) {
+    } else {
       // get user
-      $user = User::get( $req->getParam( 'user_token' ) );
+      $this->user = User::get( $req->getParam( 'user_token' ) );
 
       // test permissions
-      if ( ! $user->hasAnyAccess( $this->permission ) ) {
-        $output->add([
-          'error'   => 'permission.not_granted'
+      if ( ! is_null( $this->permission ) && ! $this->user->hasAnyAccess( $this->permission ) ) {
+        $this->output->add([
+          'error' => 'permission.not_granted'
         , 'details' => [ 'requirement' => $this->permission ]
         ])->status( 401 );
       }
     }
 
+    // test custom validations
+    $this->validate( $req, $res );
+
     // passes
-    if ( $output->ok() ) {
+    if ( $this->output->ok() ) {
       return $next( $req, $res );
     }
 
     // error
-    return $this->error( $req, $res, $output );
+    return $this->error( $req, $res );
   }
 
 
+  // Allow custom validations
+  protected function validate( $req, $res ) {}
+
+
   // Render error output
-  protected function error( $req, $res, $output ) {
+  protected function error( $req, $res ) {
     return $res
-      ->withHeader( 'Content-type', $output->mime() )
-      ->withStatus( $output->status() )
-      ->write( $output->render() );
+      ->withHeader( 'Content-type', $this->output->mime() )
+      ->withStatus( $this->output->status() )
+      ->write( $this->output->render() );
   }
 
 }
