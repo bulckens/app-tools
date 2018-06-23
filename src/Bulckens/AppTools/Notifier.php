3eb1b757cb8eb87311daa31a4411aad7c18bceef
W <wout@users.noreply.github.com>
2018-06-23 18:58:43 +0200

    Fixing log path

--- src/Bulckens/AppTools/Notifier.php
+++ src/Bulckens/AppTools/Notifier.php
@@ -19,25 +19,24 @@ class Notifier {
 
   public function __construct() {
     // catch both errors and exceptions
-    if ( ! App::env( 'dev' ) ) {
+    if ( ! App::env( 'dev' )) {
       set_error_handler( 'Bulckens\AppTools\Notifier\ErrorHandler::notify' );
       set_exception_handler( 'Bulckens\AppTools\Notifier\ErrorHandler::notify' );
     }
 
     // logger settings
     $env   = App::env();
-    $root  = App::root();
-    $dir   = "$root/log";
-    $file  = "$dir/$env.log";
+    $dir   = App::root( 'log' );
+    $file  = App::root( "log/$env.log" );
     $level = $this->config( 'level' ) ?: 'DEBUG';
 
     // make sure directory exists
-    if ( ! file_exists( $dir ) ) {
+    if ( ! file_exists( $dir )) {
       mkdir( $dir, 0777, true );
     }
 
     // initialize handler
-    $handler = new StreamHandler( $file, constant( "Monolog\Logger::$level" ) );
+    $handler = new StreamHandler( $file, constant( "Monolog\Logger::$level" ));
 
     // initialize logger
     $this->logger = new Logger( 'app_tools_logger' );
@@ -47,7 +46,7 @@ class Notifier {
 
   // Get/set arbitrary application data
   public function data( $data = null ) {
-    if ( is_null( $data ) )
+    if ( is_null( $data ))
       return $this->data;
 
     $this->data = array_replace_recursive( $this->data, $data );
@@ -58,49 +57,48 @@ class Notifier {
 
   // Log message by type
   public function log( $message, $type = 'info' ) {
-    if ( is_callable( [ $this, $type ] ) )
+    if ( is_callable( [ $this, $type ] ))
       call_user_func( [ $this, $type ], $message );
     else
       throw new NotifierMissingMethodException( "Method $type() does not exist" );
-      
+
   }
 
 
   // Send info
   public function info( $message ) {
-    $this->logger->addInfo( Style::end( Style::grey( StringHelper::stringify( $message ) ) ) );
+    $this->logger->addInfo( Style::end( Style::grey( StringHelper::stringify( $message ))));
   }
 
 
   // Send highlight
   public function high( $message ) {
-    $this->logger->addInfo( Style::end( Style::green( StringHelper::stringify( $message ) ) ) );
+    $this->logger->addInfo( Style::end( Style::green( StringHelper::stringify( $message ))));
   }
 
 
   // Send warning
   public function warn( $message ) {
-    $this->logger->warning( Style::end( Style::yellow( StringHelper::stringify( $message ) ) ) );
+    $this->logger->warning( Style::end( Style::yellow( StringHelper::stringify( $message ))));
   }
 
 
   // Send error
   public function error( $error ) {
     // if no exception is passed, make one
-    if ( is_string( $error ) )
+    if ( is_string( $error ))
       $error = new Exception( $error );
-    
+
     // mail message
     new Notification( $error, $this );
-    
+
     // log error
-    $this->logger->error( Style::end( Style::red( $error->getMessage() ) ) );
+    $this->logger->error( Style::end( Style::red( $error->getMessage())));
   }
 
-  
+
 }
 
 
 // Exceptions
 class NotifierMissingMethodException extends Exception {}
-
